202a5f6fe09d5734877fc91415f4d2cc914688c5
Shrey Singhal <shreynbd12@gmail.com>
2025-03-04 01:57:55 +0530

    Import PostForm and PostList in App.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import PostForm from "./components/PostForm"
+import PostList from "./components/PostList"
 
 function App() {
 
   return (
-    <div>
-
+    <div className="max-w-xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Social App</h1>
+      <PostForm />
+      <PostList />
     </div>
   )
 }
