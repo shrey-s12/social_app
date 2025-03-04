53d5d176a896862180ed3dbcdef7826b7c13bdbe
Shrey Singhal <shreynbd12@gmail.com>
2025-03-04 14:26:41 +0530

    saving posts in localStorage so that it persists whenever we load the page again

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { loadPosts } from "./slices/postSlice";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(loadPosts());
+  }, [])
+
   return (
     <>
       <Routes>
--- src/slices/postSlice.js
+++ src/slices/postSlice.js
@@ -1,7 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loadPosts = createAsyncThunk("posts/loadPosts", async () => {
+  const storedPosts = localStorage.getItem("posts");
+  return storedPosts ? JSON.parse(storedPosts) : [];
+});
 
 const initialState = {
   posts: [],
+  isLoading: false,
 };
 
 const postSlice = createSlice({
@@ -10,15 +16,30 @@ const postSlice = createSlice({
   reducers: {
     addPost: (state, action) => {
       state.posts.push(action.payload);
+      localStorage.setItem("posts", JSON.stringify(state.posts));
     },
     editPost: (state, action) => {
       const { id, text, image } = action.payload;
-      const post = state.posts.find(post => post.id === id);
+      const post = state.posts.find((post) => post.id === id);
       if (post) {
         post.text = text;
         post.image = image;
+        localStorage.setItem("posts", JSON.stringify(state.posts)); // Update localStorage
       }
-    }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadPosts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loadPosts.fulfilled, (state, action) => {
+        state.posts = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(loadPosts.rejected, (state) => {
+        state.isLoading = false;
+      });
   },
 });
 
