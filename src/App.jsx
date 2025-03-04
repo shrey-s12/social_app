89abd8b032baf429f13ac0a6f2bdfbb690145c71
Shrey Singhal <shreynbd12@gmail.com>
2025-03-04 17:35:28 +0530

    Import ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  
+
   return (
     <>
+      <ToastContainer position="bottom-right" autoClose={3000} />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
